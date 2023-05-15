2685869ee3cefb8749f3e829c524bf52ed64773b
deniljohn27 <117897719+deniljohn27@users.noreply.github.com>
2023-05-15 12:20:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
+let code={
+	"2":"abc", "3":"def", "4":"ghi", "5":"jkl", "6":"mno", "7":"pqrs", "8":"tu"v, "9":"wxyz"
+}
+let result=[];
 function letterCombinations(input_digit) {
   //Complete the function
+	solve(str(input_digit),0,"")
 }
+function solve(input_string,index,calculatedString){
+	if(index>=input_string.length){
+		result.push(calculatedString);
+		return;
+	}
+	let currString=mapping[input_string[index]];
+	for(let i=0;i<currString.length;i++){
+		solve(input_string,index+1,calculatedString+currString[i]);
+	}
+}
+
 
 module.exports = letterCombinations;
